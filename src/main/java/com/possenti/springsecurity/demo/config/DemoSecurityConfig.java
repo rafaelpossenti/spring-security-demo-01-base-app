8af11c5d168ede7c71b769e9617679056afb5a10
Rafael Possenti <rafael_possenti@hotmail.com>
2018-04-21 00:34:59 -0200

    custom form login

--- src/main/java/com/possenti/springsecurity/demo/config/DemoSecurityConfig.java
+++ src/main/java/com/possenti/springsecurity/demo/config/DemoSecurityConfig.java
@@ -2,6 +2,7 @@ package com.possenti.springsecurity.demo.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
@@ -23,5 +24,18 @@ public class DemoSecurityConfig extends WebSecurityConfigurerAdapter {
 			;
 		
 	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+			.anyRequest()
+			.authenticated()
+			.and()
+			.formLogin()
+			.loginPage("/showMyLoginPage")
+			.loginProcessingUrl("/authenticateTheUser")
+			.permitAll();
+	}
+	
 	
 }
