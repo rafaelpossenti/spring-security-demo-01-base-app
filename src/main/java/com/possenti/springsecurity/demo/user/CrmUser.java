2c814e9522653ce0e428ecd1285446edde6c7d83
Rafael Possenti <rafael_possenti@hotmail.com>
2018-04-26 17:21:46 -0200

    user registration completed

--- src/main/java/com/possenti/springsecurity/demo/user/CrmUser.java
+++ src/main/java/com/possenti/springsecurity/demo/user/CrmUser.java
@@ -12,8 +12,26 @@ public class CrmUser {
 	@NotNull(message="is required")
 	@Size(min=1, message="is required")
 	private String password;
-	
-	// constructor, getters/setters omitted for brevity
+
+	public CrmUser() {
+		
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
 	
 	
 }
